1e001610ee0e4f0d2edcdd2d6dfae8df2e6c31b4
Adomas Šapoka <adomas.sapoka@gmail.com>
2026-05-14 16:03:12 +0300

    resize, reserve

--- vector.h
+++ vector.h
@@ -15,6 +15,8 @@ class Vector {
         ~Vector();
 
         void push_back(const T& value);
+        void reserve (size_t newCap);
+        void resize(size_t newSiz);
 
         size_t size() const;
         size_t capacity() const;
@@ -29,6 +31,42 @@ template <typename T>
 Vector<T>::~Vector() {
     delete[] data;
 }
+
+template <typename T>
+void Vector<T>::reserve(size_t newCap)
+{
+    if (newCap <= cap) return;
+
+    T* newData = new T[newCap];
+
+    for (size_t i = 0; i < siz; i++) {
+        newData[i] = data[i];
+    }
+
+    delete[] data;
+    data = newData;
+    cap = newCap;
+}
+
+template <typename T>
+void Vector<T>::resize(size_t newSize)
+{
+    if (newSize < siz) {
+        siz = newSize;
+        return;
+    }
+
+    if (newSize > cap) {
+        reserve(newSize);
+    }
+
+    for (size_t i = siz; i < newSize; i++) {
+        data[i] = T();
+    }
+
+    siz = newSize;
+}
+
 
 
 template <typename T>
